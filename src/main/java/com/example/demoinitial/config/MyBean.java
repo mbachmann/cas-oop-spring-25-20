281171cd7403b446c55988fc97b01ce6d746e6ef
mbachmann <m.bachmann@united.ch>
2025-11-07 20:08:53 +0100

    nach Lernaufgabe LE06-3

--- src/main/java/com/example/demoinitial/config/MyBean.java
+++ src/main/java/com/example/demoinitial/config/MyBean.java
@@ -1,12 +1,32 @@
 package com.example.demoinitial.config;
 
+import com.example.demoinitial.domain.Person;
+import com.example.demoinitial.utils.HasLogger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 @Component
-public class MyBean implements CommandLineRunner {
+public class MyBean implements CommandLineRunner, HasLogger {
+
+    MyComponent myComponent;
+
+    Person felixMuster;
+    Person maxMustermann;
+
+    @Autowired
+    public MyBean(MyComponent myComponent, Person maxMustermann, Person felixMuster) {
+        this.maxMustermann = maxMustermann;
+        this.felixMuster = felixMuster;
+        this.myComponent = myComponent;
+    }
+
     @Override
     public void run(String... args) throws Exception {
-        System.out.println("CommandLineRunner started");
+        myComponent.hello();
+        getLogger().info(myComponent.getTestValue());
+        getLogger().debug("Felix Muster Test {} ", felixMuster.toString());
+        getLogger().debug("Max Mustermann Test " + maxMustermann.toString());
+        getLogger().info("CommandLineRunner started");
     }
 }
